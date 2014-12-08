8efe92e65783cd500d11f26b35423c6e06c6f166
troy betz <troybetz@gmail.com>
2014-12-08 09:10:10 -0600

    update example

--- example/example.js
+++ example/example.js
@@ -9,3 +9,63 @@ var Vimeo = require('../');
  */
 
 window.player = new Vimeo('vimeo-embed');
+
+/**
+ * Event handlers
+ */
+
+player.on('ready', function() {
+  console.log('READY');
+  console.log('player: ', player);
+
+  /**
+   * Bind button controls
+   */
+  
+  bindPlay(player);
+  bindPause(player);
+  bindDestroy(player);
+});
+
+player.on('play', function() {
+  console.log('PLAYING');
+});
+
+player.on('pause', function() {
+  console.log('PAUSED');
+});
+
+player.on('end', function() {
+  console.log('ENDED');
+});
+
+/**
+ * Player controls
+ */
+
+
+function bindPlay(player) {
+  var play = document.getElementById('play');
+
+  play.addEventListener('click', function() {
+    player.play();
+  });
+}
+
+function bindPause(player) {
+  var pause = document.getElementById('pause');
+
+  pause.addEventListener('click', function() {
+    player.pause();
+  });
+}
+
+function bindDestroy(player) {
+  var destroy = document.getElementById('destroy');
+
+  destroy.addEventListener('click', function() {
+    player.destroy();
+  });
+}
+
+
